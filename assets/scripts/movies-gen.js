57d7e5daeafa0fd2321b7443a17f75b6ef92ebdb
DariusReyesEDU <dariusreyes@cfedundrum.com>
2019-12-13 22:34:40 +0000

    Updating contents...

---
+++ assets/scripts/movies-gen.js
@@ -0,0 +1,43 @@
+(function() {
+    // Purpose of this script is to render a page, based on the ID within the URL.
+
+    // data.json should be refactored rathering than having to create another index...
+
+    var path = window.location.pathname;
+    path = path.split("/").slice(0, -1).pop(-2);
+
+    movieIndex = {
+	123: 0,
+	456: 1
+    }
+    
+    var body = document.querySelector("body"), movieContain = document.querySelector(".movie-select"), movieInfo = document.querySelector(".movie-info"), movieTitle = document.querySelector(".movie-info h2"), movieSyp = document.querySelector(".movie-info .syp"), movieFeat = document.querySelector(".movie-info .movie-ftrs"), runTime = document.querySelector(".runtime");
+    
+    //    fetch("https://DariusReyesEDU.github.io/assets/data.json")
+    fetch("http://sorryboss:5000/DariusReyesEDU.github.io/assets/data.json")
+	.then(response => response.json())
+	.then(json => {
+	    movie = json.movies[movieIndex[path]];
+
+	    body.style.backgroundImage = "url('../../assets/images/banners/"+movie.id+".jpg')";
+	    movieTitle.innerHTML = movie.title;
+	    movieSyp.innerHTML = movie.desc;
+
+	    len = Object.keys(movie.stars).length;
+
+	    for (var i = 0; i < len; i++) {
+		var x = document.createElement("div"), y = document.createElement("img"), z = document.createElement("span");
+		x.setAttribute("class", "feature");
+
+		y.src = "../../assets/images/stars/"+movie.stars[i].image;
+		z.innerHTML = movie.stars[i].name;
+		
+		x.append(y);
+		x.append(z);
+		movieFeat.append(x);
+	    };
+	    
+	    runTime.innerHTML = movie.runTime.minutes+" minutes";
+
+	});
+})();
